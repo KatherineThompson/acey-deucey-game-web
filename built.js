30ea52146d5e9d3ff565e6adb76c58957c899b20
KatherineThompson <kethompson29@gmail.com>
2016-04-15 17:25:04 -0400

    Refactoring spaces

--- built.js
+++ built.js
@@ -34682,7 +34682,7 @@
 
 	angular.module("acey-deucey").directive("space", function () {
 	    return {
-	        template: "<svg class=\"space\" viewbox=\"0 0 100 350\">\n                            <polygon points=\"0,0 100,0 50,350\"/>\n                            <g class=\"white piece\" transform=\"scale(.75), translate(17, -105)\">\n                                <circle cx=\"50\" cy=\"175\" r=\"50\"/>\n                                <text font-size=\"40\" x=\"50%\" y=\"50%\" dy=\"0.3em\" text-anchor=\"middle\">5x</text>\n                            </g>\n                    </svg>",
+	        template: "<svg class=\"space\" viewbox=\"0 0 100 350\">\n                        <polygon points=\"0,0 100,0 50,350\"/>\n                        <g class=\"white piece\" transform=\"scale(.75), translate(17, -105)\">\n                            <circle cx=\"50\" cy=\"175\" r=\"50\"/>\n                            <text font-size=\"40\" x=\"50%\" y=\"50%\" dy=\"0.3em\" text-anchor=\"middle\">5x</text>\n                        </g>\n                    </svg>",
 	        link: function link(scope) {},
 	        scope: {}
 	    };

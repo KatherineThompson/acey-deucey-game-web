555a3c040ced07b4fb0d810ad9660d97607eb415
KatherineThompson <kethompson29@gmail.com>
2016-04-15 17:28:49 -0400

    Removed query from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -11,9 +11,6 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/,
             loader: "babel",
-            query: {
-                presets: ['es2015']
-            }
         }
     ]
 };

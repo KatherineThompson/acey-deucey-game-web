50240e693a0ddeb47aa427647773072a57fa89b8
KatherineThompson <kethompson29@gmail.com>
2016-04-17 16:14:16 -0400

    Fixed webpack config

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ require("foundation-apps/dist/js/foundation-apps-templates");
 
 angular.module("acey-deucey", ["foundation"]);
 
-require("babel!./acey-deucey-controller");
-require("babel!./dice");
-require("babel!./space");
-require("babel!./ad-circle");
+require("./acey-deucey-controller");
+require("./dice");
+require("./space");
+require("./ad-circle");
--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,15 @@ module.exports = {
         path: __dirname,
         filename: "built.js"
     },
-    loaders: [
-        {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: "babel",
-        }
-    ]
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel",
+                query: { presets: ["es2015"]}
+            }
+        ],
+        noParse: [/foundation-apps/, /lodash/, /angular/]
+    }
 };

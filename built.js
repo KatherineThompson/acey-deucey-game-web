8eed1f7e8ad518961ea64e19abb07f9a50121c0b
KatherineThompson <kethompson29@gmail.com>
2016-04-17 15:44:21 -0400

    Update built.js

--- built.js
+++ built.js
@@ -50733,20 +50733,18 @@
 
 	angular.module("acey-deucey").directive("space", function () {
 	    return {
-	        template: "<svg ng-class=\"orientationParams.svgClass\" viewbox=\"0 0 100 350\">\n                        <polygon ng-attr-points=\"{{orientationParams.polygonPoints}}\"/>\n                        <g class=\"white piece\" ng-attr-transform=\"scale(.75), {{orientationParams.groupTransform}}\">\n                            <circle cx=\"50\" cy=\"175\" r=\"50\"/>\n                            <text font-size=\"40\" x=\"50%\" y=\"50%\" dy=\"0.3em\" text-anchor=\"middle\">5x</text>\n                        </g>\n                    </svg>",
+	        template: "<svg class=\"space\" viewbox=\"0 0 100 350\">\n                        <polygon ng-attr-points=\"{{orientationParams.polygonPoints}}\"/>\n                        <g class=\"white piece\" ng-attr-transform=\"scale(.75), {{orientationParams.groupTransform}}\">\n                            <circle cx=\"50\" cy=\"175\" r=\"50\"/>\n                            <text font-size=\"40\" x=\"50%\" y=\"50%\" dy=\"0.3em\" text-anchor=\"middle\">5x</text>\n                        </g>\n                    </svg>",
 	        link: function link(scope, element) {
 	            if (scope.orientation === "bottom") {
 	                scope.orientationParams = {
 	                    polygonPoints: "0,350 100,350 50,0",
-	                    groupTransform: "translate(17, 220)",
-	                    svgClass: "space"
+	                    groupTransform: "translate(17, 220)"
 	                };
 	                element.addClass("bottom");
 	            } else {
 	                scope.orientationParams = {
 	                    polygonPoints: "0,0 100,0 50,350",
-	                    groupTransform: "translate(17, -105)",
-	                    svgClass: "space"
+	                    groupTransform: "translate(17, -105)"
 	                };
 	                element.addClass("top");
 	            }

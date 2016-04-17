4bf97f42fa0c8e6529ff3e079a05e7b62bc040f1
KatherineThompson <kethompson29@gmail.com>
2016-04-17 17:13:30 -0400

    Updated circle directive to use numPieces

--- app/acey-deucey-controller.js
+++ app/acey-deucey-controller.js
@@ -4,9 +4,5 @@ const angular = require("angular");
 const gameEngine = require("acey-deucey-game-engine");
 
 angular.module("acey-deucey").controller("AceyDeuceyCtrl", function($scope) {
-    $scope.buttonClick = {count: 0};
-    $scope.onButtonClick = function() {
-      $scope.buttonClick.count++;  
-    };
-    console.log(gameEngine);
+    $scope.gameState = gameEngine.getInitialGameState();
 });
--- app/ad-circle.js
+++ app/ad-circle.js
@@ -6,10 +6,13 @@ angular.module("acey-deucey").directive("adCircle", function(){
     return {
         template: `<svg class="red piece" viewbox="0 0 100 100">
                         <circle cx="50" cy="50" r="50"/>
-                        <text font-size="40" x="50%" y="50%" dy="0.3em" text-anchor="middle">5x</text>
+                        <text font-size="40" x="50%" y="50%" dy="0.3em" text-anchor="middle">{{numPieces}}x</text>
                     </svg>`,
         link: function(scope, element) {
             element.addClass("align-center grid-block");
+        },
+        scope: {
+            numPieces: "="
         }
     };    
 });

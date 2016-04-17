62e9c5f71830c9f55553a59ab23922ed65297d45
KatherineThompson <kethompson29@gmail.com>
2016-04-17 15:48:03 -0400

    Angular (#4)
    Updated space directive. Created circle directive.

--- app/space.js
+++ app/space.js
@@ -5,11 +5,29 @@ const angular = require("angular");
 angular.module("acey-deucey").directive("space", function() {
     return {
         template: `<svg class="space" viewbox="0 0 100 350">
-                        <polygon points="0,0 100,0 50,350"/>
-                        <g class="white piece" transform="scale(.75), translate(17, -105)">
+                        <polygon ng-attr-points="{{orientationParams.polygonPoints}}"/>
+                        <g class="white piece" ng-attr-transform="scale(.75), {{orientationParams.groupTransform}}">
                             <circle cx="50" cy="175" r="50"/>
                             <text font-size="40" x="50%" y="50%" dy="0.3em" text-anchor="middle">5x</text>
                         </g>
-                    </svg>`
+                    </svg>`,
+        link: function(scope, element) {
+            if (scope.orientation === "bottom") {
+                scope.orientationParams = {
+                    polygonPoints: "0,350 100,350 50,0",
+                    groupTransform: "translate(17, 220)"
+                };
+                element.addClass("bottom");
+            } else {
+                scope.orientationParams = {
+                    polygonPoints: "0,0 100,0 50,350",
+                    groupTransform: "translate(17, -105)"
+                };
+                element.addClass("top");
+            }
+        },
+        scope: {
+            orientation: "@"
+        }
     };
 });

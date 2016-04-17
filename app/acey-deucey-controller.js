f45af2273dc0618de5aaddf150a47276f3a762ea
KatherineThompson <kethompson29@gmail.com>
2016-04-17 16:58:38 -0400

    Added game engine

--- app/acey-deucey-controller.js
+++ app/acey-deucey-controller.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const angular = require("angular");
+const gameEngine = require("acey-deucey-game-engine");
 
 angular.module("acey-deucey").controller("AceyDeuceyCtrl", function($scope) {
     $scope.buttonClick = {count: 0};
     $scope.onButtonClick = function() {
       $scope.buttonClick.count++;  
     };
+    console.log(gameEngine);
 });

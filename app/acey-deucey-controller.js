38d9811bf7d3ba0575b8774981a58f75d9f63b40
KatherineThompson <kethompson29@gmail.com>
2016-04-26 14:32:18 -0400

    Adjusting piece selection for bar pieces

--- app/acey-deucey-controller.js
+++ app/acey-deucey-controller.js
@@ -6,7 +6,8 @@ const _ = require("lodash");
 
 angular.module("acey-deucey").controller("AceyDeuceyCtrl", function($scope) {
     $scope.gameState = gameEngine.getInitialGameState();
-    $scope.gameState.isPlayerOne = false;
+    // $scope.gameState.isPlayerOne = false;
+    $scope.gameState.playerOne.barPieces = 1;
     $scope.firstQuadrantIndices = _.range(0, 6);
     $scope.secondQuadrantIndices = _.range(6, 12);
     $scope.thirdQuadrantIndices = _.range(17, 11);
@@ -14,23 +15,29 @@ angular.module("acey-deucey").controller("AceyDeuceyCtrl", function($scope) {
     $scope.turnState = {
         rolls: {first: null, second: null},
         currentPiecePosition: null,
-        availableSpaces: []
+        availableSpaces: [],
+        isBar: null
     };
     $scope.selectPiece = function(index) {
         if (!$scope.turnState.rolls.first) {
           return;
         }
         $scope.turnState.currentPiecePosition = index;
+        
+        $scope.turnState.isBar = index < -1 || index > 24 ? true : false;
+        
+        const clampedIndex = _.clamp($scope.turnState.currentPiecePosition, -1, 24);
+        
         $scope.turnState.availableSpaces = gameEngine.findPossibleMoves(
             $scope.gameState,
             _.values($scope.turnState.rolls),
-            false,
-            $scope.turnState.currentPiecePosition
+            $scope.turnState.isBar,
+            clampedIndex
         );
         debugger;
     };
     $scope.isSpaceDisabled = index => !_.includes($scope.turnState.availableSpaces, index);
-    $scope.isPieceSelectable = (indexOrPlayerName, numPieces) => {
+    $scope.isPieceSelectable = (indexOrPlayerName) => {
         let isCorrectPlayer = null;
         if (_.isBoolean(indexOrPlayerName)) {
             isCorrectPlayer = indexOrPlayerName === $scope.gameState.isPlayerOne;
@@ -38,6 +45,6 @@ angular.module("acey-deucey").controller("AceyDeuceyCtrl", function($scope) {
             isCorrectPlayer = $scope.gameState.board[indexOrPlayerName].isPlayerOne === $scope.gameState.isPlayerOne;
         }
         
-        return $scope.turnState.rolls.first && numPieces && isCorrectPlayer;
+        return $scope.turnState.rolls.first && isCorrectPlayer;
     };
 });

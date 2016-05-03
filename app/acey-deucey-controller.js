5f44cb95723e6454110ec52da058e2256060dc41
KatherineThompson <kethompson29@gmail.com>
2016-05-03 10:15:06 -0400

    Refactoring ad-circle and controller

--- app/acey-deucey-controller.js
+++ app/acey-deucey-controller.js
@@ -22,42 +22,23 @@ angular.module("acey-deucey").controller("AceyDeuceyCtrl", function($scope) {
         isBar: null
     };
     
-    $scope.selectPiece = function(index) {
-        if (!$scope.turnState.rolls.first) {
-            return;
-        }
-        
-        $scope.turnState.currentPiecePosition = index;
-        
-        $scope.turnState.isBar = !_.inRange(index, -1, 25);
-        
-        const clampedIndex = _.clamp($scope.turnState.currentPiecePosition, -1, 24);
-        
-        $scope.turnState.availableSpaces = gameEngine.findPossibleMoves(
-            $scope.gameState,
-            _.values($scope.turnState.rolls),
-            $scope.turnState.isBar,
-            clampedIndex
-        );
-    };
-    
     $scope.isSpaceDisabled = index => !_.includes($scope.turnState.availableSpaces, index);
     
-    $scope.isPieceSelectable = (indexOrPlayerName, isBar) => {
-        let isCorrectPlayer = null;
-        let pieceExists = null;
-        if (_.isBoolean(indexOrPlayerName)) {
-            isCorrectPlayer = indexOrPlayerName === $scope.gameState.isPlayerOne;
-            const activePlayer = $scope.gameState.isPlayerOne ? "playerOne" : "playerTwo";
-            if (isBar) {
-                pieceExists = $scope.gameState[activePlayer].barPieces;
-            } else {
-                pieceExists = $scope.gameState[activePlayer].initialPieces;
-            }
-        } else if (_.isNumber(indexOrPlayerName)) {
-            isCorrectPlayer = $scope.gameState.board[indexOrPlayerName].isPlayerOne === $scope.gameState.isPlayerOne;
-        }
+    // $scope.isPieceSelectable = (indexOrPlayerName, isBar) => {
+    //     let isCorrectPlayer = null;
+    //     let pieceExists = null;
+    //     if (_.isBoolean(indexOrPlayerName)) {
+    //         isCorrectPlayer = indexOrPlayerName === $scope.gameState.isPlayerOne;
+    //         const activePlayer = $scope.gameState.isPlayerOne ? "playerOne" : "playerTwo";
+    //         if (isBar) {
+    //             pieceExists = $scope.gameState[activePlayer].barPieces;
+    //         } else {
+    //             pieceExists = $scope.gameState[activePlayer].initialPieces;
+    //         }
+    //     } else if (_.isNumber(indexOrPlayerName)) {
+    //         isCorrectPlayer = $scope.gameState.board[indexOrPlayerName].isPlayerOne === $scope.gameState.isPlayerOne;
+    //     }
         
-        return $scope.turnState.rolls.first && isCorrectPlayer && pieceExists;
-    };
+    //     return $scope.turnState.rolls.first && isCorrectPlayer && pieceExists;
+    // };
 });
--- app/ad-circle.js
+++ app/ad-circle.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const angular = require("angular");
+const _ = require("lodash");
+const gameEngine = require("acey-deucey-game-engine");
 
 angular.module("acey-deucey").directive("adCircle", function(){
     return {
-        template: `<svg class="piece" ng-class="selectedClass" viewbox="0 0 100 100">
+        template: `<svg class="piece" ng-class="selectedClass" ng-click="selectPiece()" viewbox="0 0 100 100">
                         <circle cx="50" cy="50" r="50"/>
                         <text font-size="40" x="50%" y="50%" dy="0.3em" text-anchor="middle">{{numPieces}}x</text>
                     </svg>`,
@@ -13,23 +15,67 @@ angular.module("acey-deucey").directive("adCircle", function(){
             
             scope.selectedClass = {};
             
-            scope.$watch("isSelected", () => {
-                scope.selectedClass.selected = scope.isSelected;
-            });
+            scope.$watch("[turnState.currentPiecePosition, turnState.availableSpaces]", () => {
+                scope.selectedClass.selected = scope.turnState &&
+                    scope.turnState.currentPiecePosition === scope.index &&
+                    scope.turnState.availableSpaces;
+            }, true);
             
-            scope.$watch("isSelectable", () => {
-                scope.selectedClass.selectable = scope.isSelectable;
-            });
+            scope.$watch("[gameState, turnState]", () => {
+                scope.selectedClass.selectable = scope.isPieceSelectable();
+            }, true);
             
             scope.$watch("numPieces", () => {
                 scope.selectedClass.disabled = !scope.numPieces;
             });
             
+            scope.selectPiece = function() {
+                if (!_.get(scope, ["turnState", "rolls", "first"])) {
+                    return;
+                }
+                
+                scope.turnState.currentPiecePosition = scope.index;
+                
+                scope.turnState.isBar = !_.inRange(scope.index, -1, 25);
+                
+                const clampedIndex = _.clamp(scope.turnState.currentPiecePosition, -1, 24);
+                
+                scope.turnState.availableSpaces = gameEngine.findPossibleMoves(
+                    scope.gameState,
+                    _.values(scope.turnState.rolls),
+                    scope.turnState.isBar,
+                    clampedIndex
+                );
+            };
+            
+            scope.isPieceSelectable = () => {
+                let isCorrectPlayer = null;
+                let pieceExists = null;
+                if (_.isBoolean(scope.pieceIsPlayerOne)) {
+                    isCorrectPlayer = scope.pieceIsPlayerOne === scope.gameState.isPlayerOne;
+                    const activePlayer = scope.gameState.isPlayerOne ? "playerOne" : "playerTwo";
+                    if (scope.isBar) {
+                        pieceExists = scope.gameState[activePlayer].barPieces;
+                    } else {
+                        pieceExists = scope.gameState[activePlayer].initialPieces;
+                    }
+                }
+        // } else if (_.isNumber(indexOrPlayerName)) {
+        //     isCorrectPlayer = scope.gameState.board[indexOrPlayerName].isPlayerOne === scope.gameState.isPlayerOne;
+        // }
+                return _.get(scope, ["turnState", "rolls", "first"]) && isCorrectPlayer && pieceExists;
+            };            
+            
         },
+        
         scope: {
             numPieces: "=",
             isSelected: "=",
-            isSelectable: "="
+            index: "=",
+            gameState: "=",
+            turnState: "=",
+            pieceIsPlayerOne: "=",
+            isBar: "="
         }
     };    
 });

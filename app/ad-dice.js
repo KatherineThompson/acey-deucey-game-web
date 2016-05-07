77e7fd26fd77c60fec47cb23e8ec7a9a3754feaa
KatherineThompson <kethompson29@gmail.com>
2016-05-07 15:48:44 -0400

    Adding modal to choose doubles

--- app/ad-dice.js
+++ app/ad-dice.js
@@ -17,21 +17,44 @@ angular.module("acey-deucey").directive("adDice", function() {
                             </div>
                         </div>
                         <div class="align-center shrink grid-block small-offset-1" ng-if="isAceyDeucey()">
-                            <button class="centered button">Choose doubles</button>
+                            <button class="centered button"
+                            zf-open="doublesModal"
+                            ng-class="{disabled: doublesDisabled}">
+                                Choose doubles
+                            </button>
+                            <div id="doublesModal" zf-modal="">
+                                <div class="grid-block small-up-1">
+                                    <a class="close-button" zf-close="">×</a>
+                                    <div class="small-12 align-center grid-block">
+                                        <h2>Acey deucey!</h2>
+                                    </div>
+                                    <div class="small-12 align-center grid-block">
+                                        <h4>Choose your doubles:</h4>
+                                    </div>
+                                    <div class="small-12 align-spaced grid-block">
+                                        <span ng-repeat="num in [1, 2, 3, 4, 5, 6]">{{num}}</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>`,
         link: function(scope) {
             scope.rollDice = () => {
-                _.forEach(scope.rolls, roll => roll.num = _.sample(_.range(1, 7)));
+                // _.forEach(scope.rolls, roll => roll.num = _.sample(_.range(1, 7)));
+                scope.rolls[0].num = 1;
+                scope.rolls[1].num = 2;
                 
                 if (scope.rolls[0].num === scope.rolls[1].num) {
                     scope.rolls.push(_.cloneDeep(scope.rolls[0]), _.cloneDeep(scope.rolls[0]));
                 }
                 
-                scope.isAceyDeucey = function() {
-                    return _.some(scope.rolls, {num: 2}) && _.some(scope.rolls, {num: 1});
-                }
             };
+            
+            scope.isAceyDeucey = function() {
+                return _.some(scope.rolls, {num: 2}) && _.some(scope.rolls, {num: 1});
+            }
+            
+            scope.$watch("rolls", newRolls => scope.doublesDisabled = !_.every(newRolls, "used"), true);
         },
         scope: {
             rolls: "="

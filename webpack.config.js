af8cb5c5717ed2896c7b538f2915c1d8ac524c9a
KatherineThompson <kethompson29@gmail.com>
2016-05-26 19:32:06 -0400

    Consolidate sass building into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// const autoprefixer = require("autoprefixer"); 
+const path = require("path");
+
 module.exports = {
     entry: "./app/index.js",
     output: {
@@ -13,9 +16,17 @@ module.exports = {
                 exclude: /node_modules\/(?!acey-deucey-game-engine)/,
                 loader: "babel",
                 query: { presets: ["es2015"]}
+            },
+            {
+                test: /\.scss$/,
+                loaders: ["style", "css", "sass"]
             }
         ],
         noParse: [/foundation-apps/, /lodash/, /angular.js$/]
     },
+    sassLoader: {
+        includePaths: [path.join(__dirname, "node_modules", "foundation-apps", "scss")]
+    },
+    // postcss: [],
     devtool: "inline-source-map"
-};
+};  
